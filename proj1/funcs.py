d88e58e3f9b44ae0dd77045bd0a329032880ffc2
Guilherme Nunes <gmnss@protonmail.com>
2022-03-18 20:08:54 +0000

    criadas funções para medir a duração média de um algoritmo

--- proj1/funcs.py
+++ proj1/funcs.py
@@ -1,5 +1,6 @@
 import numpy
 from numpy import random
+from timeit import repeat
 
 
 # i.
@@ -11,12 +12,12 @@ from numpy import random
 def generate_random_int_list(length):
     int_list = []
     for i in range(length):
-        int_list.append(random.random_integers(1, 10))
+        int_list.append(random.random_integers(1, 100000))
     return int_list
 
 
 def generate_random_int_array(length):
-    return random.randint(1, 10, size=length)
+    return random.randint(1, 100000, size=length)
 
 
 # 2
@@ -32,3 +33,11 @@ def search_in(collection, val):
             return numpy.argmax(collection == val)
     else:
         return -1
+
+#devolve lista com durações das várias chamadas
+def get_durations(search_func, nr_of_calls):
+    return repeat(lambda: search_func, number=1, repeat=nr_of_calls)
+
+#devolve duração média dada uma lista de durações
+def get_avg_duration(duration_list: list):
+    return sum(duration_list)/len(duration_list)
